3d027fb9f3e3adea0cb02f7af515e9cd9804154d
Chris Ortman <chris-ortman@uiowa.edu>
2014-12-18 14:06:10 -0600

    Implement cloud storage

--- UIRisRetro/Model/AzureRetroItemService.cs
+++ UIRisRetro/Model/AzureRetroItemService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
 
 namespace UIRisRetro
 {
@@ -12,6 +13,38 @@ namespace UIRisRetro
 
 		public AzureRetroItemService ()
 		{
+		}
+
+		protected async Task EnsureInitialized() {
+				
+		}
+
+		protected IMobileServiceTable<RetroItem> RetroItems {
+			get { return MobileService.GetTable<RetroItem> (); }
+		}
+
+		public override async System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<RetroItem>> FindAllItems ()
+		{
+			await EnsureInitialized ();
+
+			var items = await RetroItems.CreateQuery ().ToEnumerableAsync();
+			return items;
+		}
+
+		public override async System.Threading.Tasks.Task Save (RetroItem retroItem)
+		{
+			await EnsureInitialized ();
+			if (String.IsNullOrWhiteSpace (retroItem.Id)) {
+				await RetroItems.InsertAsync (retroItem);
+			} else {
+				await RetroItems.UpdateAsync (retroItem);
+			}
+		}
+
+		public override async System.Threading.Tasks.Task Delete (RetroItem retroItem)
+		{
+			await EnsureInitialized ();
+			await RetroItems.DeleteAsync (retroItem);
 		}
 	}
 }
